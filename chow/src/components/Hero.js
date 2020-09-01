0a11ec734b4416bf91a03fbfd1f2d4aa4975b922
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-01 19:52:04 +0100

    bg--updated

--- chow/src/components/Hero.js
+++ chow/src/components/Hero.js
@@ -5,7 +5,7 @@ import React ,{ Component } from 'react';
 class Hero extends Component {
     render() {
         return(
-          <div className="hero">
+          <div className="hero row">
               <p>Get great recipes anywhere,anytime ...</p>
           </div>
         );
