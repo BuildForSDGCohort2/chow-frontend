f760a65a0b4ab7c75cdf1169271aa7aae485be66
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-02 03:46:54 +0100

    bg--fixing recipe state

--- chow/src/App.js
+++ chow/src/App.js
@@ -4,27 +4,32 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Search from "./components/Search";
-import Recipes from "./components/Recipes";
 
 
-const API_KEY="5b7f8740fa894e699119353666730f46";
+
+//const API_KEY="5b7f8740fa894e699119353666730f46";
+const API_KEY="993e24597fd724779edf491d15001a79";
+const API_ID="47862355";
 
 
 class App extends Component {
 
   state = {
-    recipes: [],
+    hits: [],
   }
 
   getRecipe = async(e) => {
     const recipeName = e.target.recipeName.value;
     e.preventDefault();
-    const apiCall= await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY}&ingredients=${recipeName}&number=10`);
+    const apiCall = await fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
+    //const apiCall= await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY}&ingredients=${recipeName}&number=10`);
     
     const data = await apiCall.json();
-    //console.log(data[0].title);
-    this.setState({ recipes: data });
-    console.log(this.state.recipes);
+    //console.log(data.hits[0].recipe);
+    this.setState({ hits: data.hits });
+    console.log(this.state.hits);
+    
+    
   }
 
   render() {
@@ -33,7 +38,9 @@ class App extends Component {
         <Header />
         <Hero />
         <Search getRecipe={this.getRecipe}/>
-        <Recipes recipes={this.state.recipes}/>
+        { this.state.hits.map((hit) => {
+          return <p key={hit.recipe.uri}>{hit.recipe.label}</p>
+        }) }
         <Footer />
       </div>
     );
