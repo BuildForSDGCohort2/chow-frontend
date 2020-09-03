930804dba9105b5fc97ce42bd4efae6a3d878b0f
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-03 08:15:58 +0100

    bg--security vulnerabilities fixed

--- chow/src/App.js
+++ chow/src/App.js
@@ -9,8 +9,8 @@ import Recipes from "./components/Recipes";
 
 
 //const API_KEY="5b7f8740fa894e699119353666730f46";
-const API_KEY="993e24597fd724779edf491d15001a79";
-const API_ID="47862355";
+const API_KEY=process.env.REACT_APP_API_KEY;
+const API_ID=process.env.REACT_APP_API_ID;
 
 
 class App extends Component {
