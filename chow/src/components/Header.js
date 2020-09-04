836633b8320485714ca13ccb59828a25963aee32
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-04 05:31:12 +0100

    bg--header updated

--- chow/src/components/Header.js
+++ chow/src/components/Header.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-    render() {
-        return(
-            <div className="header">
-                <nav>
-                    <a className="brand-logo" href="#">choW</a>
-                </nav>
-            </div>
-        );
-    }
-}
+const Header = () => {
+    return(
+        <div className="header">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">choW</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+
+} 
+        
 
 export default Header;
