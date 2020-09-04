4e7e662b49352951fb6e39f17d039e3708be1bf7
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-04 05:58:32 +0100

    bg--updated header

--- chow/src/components/Header.js
+++ chow/src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Header = () => {
     return(
-        <div className="header">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="row">
+            <nav className="navbar navbar-expand-lg navbar-light  header">
                 <a className="navbar-brand" href="#">choW</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
+                    <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <a className="nav-link" href="#">Home</a>
                         </li>
