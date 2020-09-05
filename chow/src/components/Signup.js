be0605743e4fffd4fd97469f3c9aade47f2c2f2b
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-05 12:54:52 +0100

    bg--updated signup

--- chow/src/components/Signup.js
+++ chow/src/components/Signup.js
@@ -1,10 +1,32 @@
 import React from "react";
+import Footer from "./Footer";
 
 const SignUp = () => {
     return(
-        <div>
-            <h3>Sign Up Component</h3>
-            <p>Work in  Progress</p>
+        <div className="row text-center">
+            <div className="col-sm-12">
+                <h1 className="pl-4 pb-3">choW</h1>
+            </div>
+            <div className="col-sm-12">
+                <ul className="connect">
+                    <li>
+                        <button className="facebook m-2">
+                            <span>Connect with Facebook</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="google m-2">
+                            <span>Connect with Google</span>
+                        </button>
+                    </li>
+                    <li>
+                        <button className="email m-2">
+                            <span>Connect with Email</span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
+            <Footer />
         </div>
     );
 };
