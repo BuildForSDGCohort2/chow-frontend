e7d2f3cc02471882316af4a05f6433b27288ba2b
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-05 21:00:13 +0100

    ft--signup with email updated

--- chow/src/components/EmailSignUp.js
+++ chow/src/components/EmailSignUp.js
@@ -10,19 +10,19 @@ const EmailSignUp = () => {
                 <form>
                     <div>
                        <label htmlFor="username"></label>
-                       <input type="text" name="username"className="username" placeholder="John Doe" /> 
+                       <input type="text" name="username"className="username pl-5" placeholder="John Doe" /> 
                     </div>
                     <div>
                        <label htmlFor="email"></label>
-                       <input type="email" name="email" className="email2" placeholder="johndoe@example.com" /> 
+                       <input type="email" name="email" className="email2 pl-5" placeholder="johndoe@example.com" /> 
                     </div>
                     <div>
                        <label htmlFor="password1"></label>
-                       <input type="password" name="password1" className="pswd" placeholder="" /> 
+                       <input type="password" name="password1" className="pswd pl-5" placeholder="" /> 
                     </div>
                     <div>
                        <label htmlFor="password2"></label>
-                       <input type="password" name="password2" className="pswd" placeholder="" /> 
+                       <input type="password" name="password2" className="pswd pl-5" placeholder="" /> 
                     </div>
                     <button className="btn btn-primary">Register</button>
                 </form>
