3a5d2d95dfa9cd2aeb619facfc30af77300622f6
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-05 13:39:24 +0100

    bg--updated signup

--- chow/src/components/Signup.js
+++ chow/src/components/Signup.js
@@ -6,26 +6,29 @@ const SignUp = () => {
     return(
         <div className="row text-center">
             <div className="col-sm-12">
-                <h1 className="pl-4 pb-3">choW</h1>
+                <h1 className="heading">choW</h1>
             </div>
             <div className="col-sm-12">
                 <ul className="connect">
                     <li>
-                        <button className="facebook m-2">
+                        <button className="facebook m-2" title="Connect with Facebook">
                             <span>Connect with Facebook</span>
                         </button>
                     </li>
                     <li>
-                        <button className="google m-2">
+                        <button className="google m-2" title="Connect with Google">
                             <span>Connect with Google</span>
                         </button>
                     </li>
                     <li>
-                        <button className="email m-2">
+                        <button className="email m-2" title="Connect with Email">
                             <span>Connect with Email</span>
                         </button>
                     </li>
                 </ul>
+            </div>
+            <div className="col-sm-12">
+                <small className="text center">By connecting, you agree to our Terms of Use and Privacy Notice</small>
             </div>
             <Footer />
         </div>
