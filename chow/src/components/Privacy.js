5102e76892b622fe3c92a01679f364552ab4d373
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-10 04:56:02 +0100

    ft--privacy policy added && codacy fixed

--- chow/src/components/Privacy.js
+++ chow/src/components/Privacy.js
@@ -83,7 +83,7 @@ const Privacy = () => {
           <p>choW does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
         </div>
     </div>
-  )
-}
+  );
+};
 
 export default Privacy;
