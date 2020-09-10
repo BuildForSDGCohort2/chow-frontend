bcc82db486f6519ba857e6e7a0a1303e96739310
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-10 05:29:11 +0100

    ft--added terms and conditions route to router

--- chow/src/components/Router.js
+++ chow/src/components/Router.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "../App";
 import Recipe from "./Recipe";
+import Terms from "./Terms";
 
 const Router = () => {
     <BrowserRouter>
         <Switch>
             <Route path="/" component={App} exact/>
             <Route path="/recipe/:uri" component={Recipe}/>
+            <Route path="/terms" component={Terms} />
         </Switch>
     </BrowserRouter>;
 };
