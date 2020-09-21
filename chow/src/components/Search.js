e563a7cf75afd1ecb3371790c940b34a455aa37b
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-21 07:08:05 +0100

    bg--validation for search

--- chow/src/components/Search.js
+++ chow/src/components/Search.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 
 const Search = (props) => {
+    const { register, handleSubmit, errors } = useForm()
     return(
         <div className="search">
             <form className="form" onSubmit={props.getRecipe} >
@@ -10,8 +12,17 @@ const Search = (props) => {
                     name="recipeName"
                     id="search"
                     placeholder="E.g Chicken"
+                    ref={ register({
+                        required: "Search field is required",
+                        pattern: {
+                            value: /^[A-Za-z]+$/i,
+                            message: "Search needs to be Upper or lower case letters"
+                        }
+                    })}
+                    style={{ borderColor: errors.recipeName && "red" }}
                 />
-                <button className="btn">Search Recipes</button>
+                { errors.recipeName && <p>{errors.recipeName.message}</p> }
+                <button className="btn" type="submit">Search Recipes</button>
             </form>
         </div>
     );
