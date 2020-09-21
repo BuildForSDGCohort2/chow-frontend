7cf0828020606bd892b17ce76ee36c8cb70d5127
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-21 11:36:51 +0100

    bg--updated

--- chow/src/components/Search.js
+++ chow/src/components/Search.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import { useForm } from "react-hook-form";
 
 
 const Search = (props) => {
-    const { register, handleSubmit, errors } = useForm()
     return(
-        <div className="search">
+        <div className="search m-3">
             <form className="form" onSubmit={props.getRecipe} >
                 <input 
                     type="text"
                     name="recipeName"
                     id="search"
                     placeholder="E.g Chicken"
-                    ref={ register({
-                        required: "Search field is required",
-                        pattern: {
-                            value: /^[A-Za-z]+$/i,
-                            message: "Search needs to be Upper or lower case letters"
-                        }
-                    })}
-                    style={{ borderColor: errors.recipeName && "red" }}
+                    required
                 />
-                { errors.recipeName && <p>{errors.recipeName.message}</p> }
                 <button className="btn" type="submit">Search Recipes</button>
             </form>
         </div>
