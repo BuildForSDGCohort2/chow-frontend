b62a62d08b7d089b8752f0424e22dd84dfe870d2
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-23 07:08:58 +0100

    ft--carusel added to hero

--- chow/src/components/Hero.js
+++ chow/src/components/Hero.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { useState } from "react";
+import { Carousel } from "react-bootstrap";
+import CaroselImg1 from "../images/anna-pelzer-IGfIGP5ONV0-unsplash-removebg-preview.png";
+import CaroselImg2 from "../images/brooke-lark-08bOYnH_r_E-unsplash-removebg(1).png";
+import CaroselImg3 from "../images/baehaki-hariri-yBeL0XChO8o-unsplash-removebg-preview.png";
 
 
 const Hero = () => {
+    const [ index, setIndex ] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex)
+    }
     return(
-        <div className="hero row">
-            <div className="col-lg-6 col-md-6 col-sm-12">
-                <p>Get great recipes <br/>anywhere,anytime ...</p>
+        <div className="row">
+            <div className="col-sm-12">
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+              <Carousel.Item>
+                  <img 
+                    className="img-fluid"
+                    src={ CaroselImg1 }
+                  />
+              </Carousel.Item>
+              <Carousel.Caption>
+                  
+              </Carousel.Caption>
+              <Carousel.Item>
+                  <img
+                    className="img-fluid"
+                    src={ CaroselImg2 }
+                  />
+              </Carousel.Item>
+              <Carousel.Caption>
+                  
+              </Carousel.Caption>
+              <Carousel.Item>
+                  <img
+                    className="img-fluid"
+                    src={ CaroselImg3 }
+                  />
+              </Carousel.Item>
+              <Carousel.Caption>
+                 
+              </Carousel.Caption>
+          </Carousel>
             </div>
         </div>
       );
