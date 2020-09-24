0e43031358467f5b1581c885bfea113a727eee0a
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-24 21:25:02 +0100

    ft--google authentication fro user signup added

--- chow/src/components/Signup.js
+++ chow/src/components/Signup.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import { GoogleLogin } from "react-google-login/";
 import Footer from "./Footer";
 import Heading from "./Heading";
 
@@ -12,14 +13,21 @@ const SignUp = () => {
     }
 
     const componentClicked = () => {
-        console.log( "Clicked!" )
-    }
+        console.log( "Clicked!" );
+    };
+
+    const onSuccess = (response) => {
+        console.log("[Login Success] currentuser", response.profileObj);
+    };
+
+    const onFailure = (response) => {
+        console.log("[Login failed] response:", response);
+    };
 
     return(
         <div className="row text-center">
             <div className="col-sm-12 signUp">
             <Heading />
-             
                 <ul className="connect">
                     <li>
                     <FacebookLogin 
@@ -27,12 +35,12 @@ const SignUp = () => {
                       autoLoad={true}
                       cookie={true}
                       version="8.0"
+                      status={true}
                       scope="public_profile,email"
                       redirectUri="/dashboard"
                       fields="name,email,picture"
                       onClick={componentClicked}
                       callback={responseFacebook}
-                      
                       render={
                            renderProps => (
                                <button onClick={renderProps.onClick} className="facebook m-2" title="Connect with Facebook">Connect with Facebook</button>
@@ -41,9 +49,19 @@ const SignUp = () => {
                     />
                     </li>
                     <li>
-                      <button className="google m-2" title="Connect with Google">
-                          <span>Connect with Google</span>
-                      </button>
+                      <GoogleLogin 
+                        clientId="619240812460-jbjbp3rditc3e3kp54fgqb3l2nrnd2u3.apps.googleusercontent.com"
+                        buttonText="Connect with Google"
+                        onSuccess={onSuccess}
+                        onFailure={onFailure}
+                        cookiePolicy={ "single_host_origin" }
+                        isSignedIn={true}
+                        render={
+                            renderProps => (
+                                <button  onClick={renderProps.onClick} className="google m-2">Connect with Google</button>
+                            )
+                        }
+                      />
                     </li>
                     <Link to="/email-signup">
                       <button className="email m-2" title="Connect with Email">
