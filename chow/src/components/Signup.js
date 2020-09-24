c5ac544811d22885226c20fd966e49406b0f0f15
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-24 19:45:40 +0100

    ft--Facebook authentication added

--- chow/src/components/Signup.js
+++ chow/src/components/Signup.js
@@ -1,29 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 import Footer from "./Footer";
 import Heading from "./Heading";
 
 
 const SignUp = () => {
+
+    const responseFacebook = (response) => {
+        console.log(response);
+    }
+
+    const componentClicked = () => {
+        console.log( "Clicked!" )
+    }
+
     return(
         <div className="row text-center">
             <div className="col-sm-12 signUp">
             <Heading />
+             
                 <ul className="connect">
                     <li>
-                        <button className="facebook m-2" title="Connect with Facebook">
-                            <span>Connect with Facebook</span>
-                        </button>
+                    <FacebookLogin 
+                      appId="240858893885753"
+                      autoLoad={true}
+                      cookie={true}
+                      version="8.0"
+                      scope="public_profile,email"
+                      redirectUri="/dashboard"
+                      fields="name,email,picture"
+                      onClick={componentClicked}
+                      callback={responseFacebook}
+                      
+                      render={
+                           renderProps => (
+                               <button onClick={renderProps.onClick} className="facebook m-2" title="Connect with Facebook">Connect with Facebook</button>
+                           )
+                      }
+                    />
                     </li>
                     <li>
-                        <button className="google m-2" title="Connect with Google">
-                            <span>Connect with Google</span>
-                        </button>
+                      <button className="google m-2" title="Connect with Google">
+                          <span>Connect with Google</span>
+                      </button>
                     </li>
                     <Link to="/email-signup">
-                        <button className="email m-2" title="Connect with Email">
-                            <span>Connect with Email</span>
-                        </button>
+                      <button className="email m-2" title="Connect with Email">
+                          <span>Connect with Email</span>
+                      </button>
                     </Link>
                 </ul>
                 <Link to="/">
