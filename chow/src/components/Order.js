8826f4154592ce78444f0930d39a18de6e8050a9
Kingsley <kingsleyumujeyan@gmail.com>
2020-09-26 07:29:06 +0100

    ft--firebase added

--- chow/src/components/Order.js
+++ chow/src/components/Order.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Search from "./Search";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -7,8 +8,11 @@ const Order = () => {
         <div className="container-fluid">
           <Header />
           <div className="row">
-            <div className="col-sm-12 order text-center mt-5">
+            <div className="col-sm-12 myOrder text-center mt-5">
                 <h2>Order Page</h2>
+               <Search 
+                className="searchFood"
+               />
             </div>
           </div>
           <Footer />
