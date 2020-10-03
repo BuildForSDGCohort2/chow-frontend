5a202d92b485bbb91fee8bc02070db87423d2d9a
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-03 20:09:27 +0100

    bg--updated

--- chow/src/components/MealIdeas.js
+++ chow/src/components/MealIdeas.js
@@ -13,8 +13,8 @@ const MealIdeas = () => {
         <div className="container-fluid">
           <Header />
           <div className="row">
-            <div className="col-sm-6 myOrder"></div>
-            <div className="col-sm-6 meal-plan text-justify mt-5">
+            <div className="col-sm-12 col-lg-6 myOrder"></div>
+            <div className="col-sm-12 col-lg-6 meal-plan text-justify mt-5">
                 <h2 className="text-center">Create Your Plan</h2>
                 <p>Ready to choose your meals! Use the + icon to add recipes to your plan.For exciting new meals personalized to your taste preferences,<br/>Check our recommendations.
                   or your saved recipes.
@@ -43,16 +43,16 @@ const MealIdeas = () => {
                        <h5>Recipes Choices</h5>
                        <div className="row">
                          <div className="col-sm-12 col-lg-4 content content_1 m-2">
-                           <p>Chicken</p>
+                           <button className="items">Chicken</button>
                          </div>
                          <div className="col-sm-12 col-lg-4 content content_3 m-2">
-                           <p>Pasta</p>
+                           <button className="items">Pasta</button>
                          </div>
                          <div className="col-sm-12 col-lg-4 content content_2 m-2">
-                           <p>Fish</p>
+                           <button className="items">Fish</button>
                          </div>
                          <div className="col-sm-12 col-lg-4 content content_4 m-2">
-                           <p>Lamb</p>
+                           <button className="items">Lamb</button>
                          </div>
                        </div>
                      </Tab>
@@ -60,16 +60,16 @@ const MealIdeas = () => {
                        <h5>Easy Starter Menu</h5>
                        <div className="row">
                          <div className="col-sm-12 col-lg-4 content content1 m-2">
-                           <p>BreakFast</p>
+                           <button className="items">Breakfast</button>
                          </div>
                          <div className="col-sm-12 col-lg-4 content content2 m-2">
-                           <p>Lunch</p>
+                           <button className="items">Lunch</button>
                          </div>
                          <div className="col-sm-12 col-lg-4 content content3 m-2">
-                           <p>Dinner</p>
+                           <button className="items">Dinner</button>
                          </div>
                          <div className="col-sm-12 col-lg-4 content content4 m-2">
-                           <p>Fruits</p>
+                           <button className="items">Salad</button>
                          </div>
                        </div>
                      </Tab>
