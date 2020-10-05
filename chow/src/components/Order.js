207ef7c1f70e149c0e8e95e56aebba8841b8d287
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-05 07:26:21 +0100

    ft--updated order page

--- chow/src/components/Order.js
+++ chow/src/components/Order.js
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Tabs, Tab } from "react-bootstrap";
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Order = () => {
+  const [key, setKey] = useState("food", "snacks", "salads", "smoothie", "drinks", "small chops", "pizza");
+
     return (
         <div className="container-fluid">
           <Header />
           <div className="row">
-            <div className="col-sm-12 myOrder text-center">
+            <div className="col-sm-12 col-lg-5 myOrder text-center">
               <div className="my m-5 p-3">
                 <h3>Order Page</h3>
                 <p>Sorry dear,we are still working on this page.</p>
                 <Link to="/dashboard" className="go">Go Back</Link>
+              </div>  
+            </div>
+            <div className="col-sm-12 col-lg-7 text-center">
+              <h3 className="mb-2">Hungry?</h3>
+              <p>We got your Back</p>
+              <div className="row">
+                <div className="col-sm-12">
+                  <Tabs
+                  id="orders"
+                  activeKey={key}
+                  onSelect={(k) => setKey(k)}
+                  transition={false}
+                  >
+                    <Tab eventKey="food" title="Food" className="mx-3">
+                      <h5>Food Menu</h5>
+                    </Tab>
+                    <Tab eventKey="snacks" title="Snacks" className="mx-3">
+                      <h5>Snack Menu</h5>
+                    </Tab>
+                    <Tab eventKey="salads" title="Salads" className="mx-3">
+                      <h5>Salad Menu</h5>
+                    </Tab>
+                    <Tab eventKey="smoothie" title="Smoothies" className="mx-3">
+                      <h5>Smoothie Menu</h5>
+                    </Tab>
+                    <Tab eventKey="small chops" title="Small Chops" className="mx-3">
+                      <h5>Small Chops Menu</h5>
+                    </Tab>
+                    <Tab eventKey="pizza" title="Pizza" className="mx-3">
+                      <h5>Pizza Menu</h5>
+                    </Tab>
+                    <Tab eventKey="drinks" title="Drinks" className="mx-3">
+                      <h5>Drink Menu</h5>
+                    </Tab>
+                  </Tabs>
+                </div>
               </div>
-                
             </div>
           </div>
           <Footer />
