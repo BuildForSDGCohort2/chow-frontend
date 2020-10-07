45f6289789e555bd41e038a799ac1fee25bc9376
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-07 21:05:12 +0100

    ft--added salads to order

--- chow/src/components/Salads.js
+++ chow/src/components/Salads.js
@@ -2,15 +2,26 @@ import React from "react";
 import { CardGroup, CardImg, Card } from "react-bootstrap";
 import AddToCart from "./AddToCart";
 
+import CaesarImg from "../images/salads/caesar-salad-88749-1.jpeg";
+import ChickenCaesarImg from "../images/salads/chicken-caesar-salad-83105-1.jpeg";
+import GreekImg from "../images/salads/super-easy-greek-salad-81664-1.jpeg";
+import PotatoImg from "../images/salads/potato-salad-606-1.jpeg";
+import LettuceImg from "../images/salads/lettuce-avocado-and-mango-salad-59642-1.jpeg";
+import PastaImg from "../images/salads/pasta-salad-714-1.jpeg";
+
 
 const Salads = () => {
     return (
         <div className="row">
              <CardGroup>
                  <Card>
-                     <CardImg/>
+                     <CardImg
+                        src={ CaesarImg }
+                        alt=""
+                        className="prdt"
+                     />
                      <Card.Body>
-                         <Card.Title></Card.Title>
+                         <Card.Title>Caesar salad</Card.Title>
                      </Card.Body>
                      <Card.Footer>
                         <span className="from mx-1">₦</span>500
@@ -18,9 +29,13 @@ const Salads = () => {
                      </Card.Footer>
                  </Card>
                  <Card>
-                     <CardImg/>
+                     <CardImg
+                      src={ ChickenCaesarImg }
+                      alt=""
+                      className="prdt"
+                     />
                      <Card.Body>
-                         <Card.Title></Card.Title>
+                         <Card.Title>Chicken Caesar salad</Card.Title>
                      </Card.Body>
                      <Card.Footer>
                         <span className="from mx-1">₦</span>500
@@ -28,9 +43,13 @@ const Salads = () => {
                      </Card.Footer>
                  </Card>
                  <Card>
-                     <CardImg/>
+                     <CardImg
+                     src={ GreekImg }
+                     alt=""
+                     className="prdt"
+                     />
                      <Card.Body>
-                         <Card.Title></Card.Title>
+                         <Card.Title>Greek salad</Card.Title>
                      </Card.Body>
                      <Card.Footer>
                         <span className="from mx-1">₦</span>500
@@ -40,9 +59,13 @@ const Salads = () => {
              </CardGroup>
              <CardGroup>
                  <Card>
-                     <CardImg/>
+                     <CardImg
+                     src={ PotatoImg }
+                     alt=""
+                     className="prdt"
+                     />
                      <Card.Body>
-                         <Card.Title></Card.Title>
+                         <Card.Title>Potato salad</Card.Title>
                      </Card.Body>
                      <Card.Footer>
                         <span className="from mx-1">₦</span>500
@@ -50,9 +73,13 @@ const Salads = () => {
                      </Card.Footer>
                  </Card>
                  <Card>
-                     <CardImg/>
+                     <CardImg
+                     src={ PastaImg }
+                     alt=""
+                     className="prdt"
+                     />
                      <Card.Body>
-                         <Card.Title></Card.Title>
+                         <Card.Title>Pasta salad</Card.Title>
                      </Card.Body>
                      <Card.Footer>
                         <span className="from mx-1">₦</span>500
@@ -60,9 +87,12 @@ const Salads = () => {
                      </Card.Footer>
                  </Card>
                  <Card>
-                     <CardImg/>
+                     <CardImg
+                     src={ LettuceImg }
+                     alt=""
+                     className="prdt"/>
                      <Card.Body>
-                         <Card.Title></Card.Title>
+                         <Card.Title>Lettuce-Avocado salad</Card.Title>
                      </Card.Body>
                      <Card.Footer>
                         <span className="from mx-1">₦</span>500
