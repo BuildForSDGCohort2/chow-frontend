f362285a36ba98cc1190abd04ff2c9fd0f2d7584
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-08 20:45:01 +0100

    bg--updated

--- chow/src/components/Counter.js
+++ chow/src/components/Counter.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 
 const Counter = () => {
+
     const [count, setCount] = useState(1);
 
     const handleIncrement = (e) => {
@@ -20,9 +21,13 @@ const Counter = () => {
     return (
        <div className="row">
            <div className="col-sm-12 my-1 counter">
-                <button onClick={handleDecrement} className="count minus"></button>
+                <button onClick={handleDecrement} className="count minus">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                </button>
                 <span className="mx-3 countNum px-3">{count}</span>
-                <button onClick={handleIncrement} className="count plus"></button>
+                <button onClick={handleIncrement} className="count plus">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                </button>
            </div>
        </div>
     );
