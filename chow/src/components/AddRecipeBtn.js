19c2c35747ae6f8ca4badb5a0048bf07caab3498
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-10 07:48:31 +0100

    ft--updated recipe button

--- chow/src/components/AddRecipeBtn.js
+++ chow/src/components/AddRecipeBtn.js
@@ -3,15 +3,18 @@ import React, { useState } from "react";
 
 const AddRecipeBtn = () => {
 
-    const [text, setText] = useState("Add to My Recipes");
-    const [dataTip, setDataTip] = useState("Add this recipe to my Recipes")
+    const [text, setText] = useState( "Add to my Recipes" );
+    const [dataTip, setDataTip] = useState("Add this recipe to my Recipes");
+
     const addRecipe = (e) => {
         e.preventDefault();
         setTimeout(() => {
-            setText("Recipe Added!")
-            setDataTip("Recipe was Added!")
+            setText("Recipe Added");
+            setDataTip("Recipe was Added!");
+            localStorage.setItem("value", "Recipe Added");
+            
         }, 1500);
-    }
+    };
     
     return (
         <button
@@ -21,7 +24,7 @@ const AddRecipeBtn = () => {
           >
           {text}
           </button>
-    )
+    );
 }
 
 
