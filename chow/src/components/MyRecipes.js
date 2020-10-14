1551a35a25f05f797333affce5ecc4b5b9a3d2c0
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-14 20:23:27 +0100

    bg--updated myrecipe page

--- chow/src/components/MyRecipes.js
+++ chow/src/components/MyRecipes.js
@@ -20,13 +20,13 @@ const MyRecipes = () => {
    if (savedRecipes) {
      setActiveRecipe(savedRecipes);
      setIngredients(savedRecipes.ingredients);
-     console.log(savedRecipes);
+     console.log(savedRecipes.ingredients);
    }
   }, [])
 
   const cal = parseInt(activeRecipe.calories).toString();
   const calServ = (Math.round(cal / (activeRecipe.yield))).toString();
- 
+  
   
     return (
         <div className="container-fluid">
@@ -70,7 +70,7 @@ const MyRecipes = () => {
                     {ingredients.map((item, index) => (
                       <div key={index}>
                         <p className="px-2">
-                        <button data-tip="Buy Ingredients" className="itemBtn" onClick={handleShow} data-tip={` Buy ${item.text}`}>
+                        <button className="itemBtn" onClick={handleShow} data-tip={` Buy ${item.text}`}>
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#3b5998" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
                         </button>
                         <ReactToolTip
@@ -82,12 +82,14 @@ const MyRecipes = () => {
                         />
                         <span className="px-1">{item.text}</span>
                         </p>
+                        <img src={item.image} alt="" className="img-fluid itemImg" />
                       </div>
                     ))}
                   </div>
+                  
                   <Modal show={show} onHide={handleClose} >
                         <Modal.Header closeButton>
-                          <Modal.Title>Ingredient Name</Modal.Title>
+                          <Modal.Title>{}</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
                           
@@ -104,8 +106,8 @@ const MyRecipes = () => {
                             >Buy Ingredient
                           </button>
                         </Modal.Footer>
-                      </Modal> 
-                  <a className="home view">Shop Ingredients</a>
+                  </Modal>
+                  <button className="home view">Shop Ingredients</button>
                   <hr/>
                   <div>
                     <a className="url px-4 py-1" href={activeRecipe.url}>Directions</a>
