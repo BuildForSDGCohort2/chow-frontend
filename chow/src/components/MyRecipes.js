521a1e1de13df51788cf70430b72b158bbb29a75
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-14 07:50:14 +0100

    bg--updated

--- chow/src/components/MyRecipes.js
+++ chow/src/components/MyRecipes.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import ReactToolTip from "react-tooltip";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -43,7 +44,7 @@ const MyRecipes = () => {
                   <span>Source: </span>
                   <a href={activeRecipe.url}>{activeRecipe.source}</a>
                   <div>
-                    <Link to="/" className="  home view">View More</Link>
+                    <Link to="/" className="home view">More Recipes</Link>
                   </div>
                   <p>Servings: {activeRecipe.yield}</p>
                   <hr/>
@@ -63,10 +64,23 @@ const MyRecipes = () => {
                     <hr/>
                     {ingredients.map((item, index) => (
                       <div key={index}>
-                        <p>{item.text}</p>
+                        <p className="px-2">
+                        <a data-tip="Buy Ingredients">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#3b5998" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+                        </a>
+                          <span className="px-1">{item.text}</span>
+                        </p>
                       </div>
                     ))}
                   </div>
+                  <ReactToolTip
+                                  type="dark"
+                                  place="top"
+                                  effect="solid"
+                                  backgroundColor="#000"
+                                  borderColor="#fff"
+                                />
+                  <a className="home view">Shop Ingredients</a>
                   <hr/>
                   <div>
                     <a className="url px-4 py-1" href={activeRecipe.url}>Directions</a>
