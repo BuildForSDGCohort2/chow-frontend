2f1038f6c19e7081c6ce09494085d312be00d93a
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-23 20:10:43 +0100

    ft--updated

--- chow/src/components/Router.js
+++ chow/src/components/Router.js
@@ -39,7 +39,7 @@ class Router extends Component {
     <BrowserRouter>
         <Switch>
             <Route path="/" component={ App } exact/>
-            <Route path="/home" component={ App }/>
+            <Route path="/home" component={ App } exact/>
             <Route path="/recipe/:uri" component={ Recipe }/>
             <Route path="/terms" component={ Terms } />
             <Route path="/signup" component={ SignUp }/>
