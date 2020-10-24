3e9af06e86e48151ed1d24d9b41ba6931b8a2a23
Kingsley <kingsleyumujeyan@gmail.com>
2020-10-24 21:10:06 +0100

    ft--updated

--- chow/src/components/Search.js
+++ chow/src/components/Search.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { Navbar } from "react-bootstrap";
 
 const Search = (props) => {
     return(
         <div className="search m-3">
-            <form className="form" onSubmit={props.getRecipe} >
+            <form className="form" onSubmit={props.getRecipe}>
                 <input 
                     type="search"
                     name="recipeName"
