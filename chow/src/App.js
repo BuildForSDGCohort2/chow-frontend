1028e753787d2fa2e698d8cbc5d36bd947377625
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-04 05:33:12 +0100

    bg--updated

--- chow/src/App.js
+++ chow/src/App.js
@@ -13,8 +13,8 @@ import Hero from "./components/Hero";
 
 
 
-const API_KEY=process.env.REACT_APP_API_KEY || "993e24597fd724779edf491d15001a79";
-const API_ID=process.env.REACT_APP_API_ID || "47862355";
+const API_KEY=`${process.env.REACT_APP_API_KEY}`;
+const API_ID=`${process.env.REACT_APP_API_ID}`;
 
 
 class App extends Component {
