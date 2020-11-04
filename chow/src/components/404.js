37eeee6e35b994a4f17e02de0721a68826f420ce
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-04 19:11:13 +0100

    ft--updated 404

--- chow/src/components/404.js
+++ chow/src/components/404.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import { Link } from "react-router-dom";
 
 
 const NotFound = () => {
@@ -10,6 +11,11 @@ const NotFound = () => {
                 <div className="col-sm-12 not">
                     <h2>404 - Page Not Found</h2>
                     <b className="py-5">It seems the page you are looking for doesn't exist.</b>
+                    <div className="m-2">
+                        <button className="add">
+                            <Link to="/" className="add">Go Home</Link>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
