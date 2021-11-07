4fbe5c6764aa22d80e2b0370a6eadc93d0c74d4d
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2021-11-07 14:58:52 +0100

    ft--updated index.js

--- chow/src/index.js
+++ chow/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import Routing from "./components/Routing";
 // import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
-import App from './App';
+// import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
